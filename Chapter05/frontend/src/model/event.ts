c1df25b227fd5492f1040a7421da28856bddb8e9
Thanh Tran <tranthanh19589@gmail.com>
2021-03-22 00:26:43 +0700

    add chapter 05

---
+++ Chapter05/frontend/src/model/event.ts
@@ -0,0 +1,10 @@
+export interface Event {
+    ID: string;
+    Name: string;
+    StartDate: number;
+    EndDate: number;
+    Location: {
+        ID: string;
+        Name: string;
+    }
+}
---
+++ Chapter05/frontend/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    devServer: {
+        inline: false,
+        contentBase: "./dist",
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".css", ".js"]
+    },
+
+    module: {
+        // loaders: [
+        //     {
+        //         test: /\.tsc?$/,
+        //         loader: "awesome-typescript-loader"
+        //     }
+        // ],
+
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+                // loader: "ts-loader"
+            },
+            // {
+            //     test: /.jsx?$/,
+            //     loader: 'babel-loader',
+            //     query: {
+            //         presets: [['es2015', {}]]
+            //     }
+            // },
+            // {
+            //     test: /\.css$/,
+            //     loader: "style-loader!css-loader"
+            // },
+            // {
+            //     test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+            //     loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            // },
+            // {
+            //     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            //     loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            // },
+            // {
+            //     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            //     loader: 'file-loader'
+            // },
+            // {
+            //     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            //     loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+            // }
+            /*,
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            }*/
+        ]
+    },
+    plugins: [
+        new CheckerPlugin()
+    ],
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
